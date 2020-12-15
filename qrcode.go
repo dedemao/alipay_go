4162b0d5b16c638e281f1047fb4cbb50d3919e9e
dedemao <884358@qq.com>
2020-12-15 13:15:05 +0800

    当面付（扫码支付）
    增加异步回调

--- qrcode.go
+++ qrcode.go
@@ -12,6 +12,7 @@ import (
 	"encoding/pem"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
@@ -22,36 +23,33 @@ import (
 )
 
 /*** 请填写以下配置信息 ***/
-var appid string = "2021001157638209"                                    //https://open.alipay.com 账户中心->密钥管理->开放平台密钥，填写添加了电脑网站支付的应用的APPID
-var returnUrl string = "https://www.dedemao.com/alipay/return.php"       //付款成功后的同步回调地址
-var notifyUrl string = "https://www.dedemao.com/alipay/notify_redis.php" //付款成功后的异步回调地址
-var outTradeNo string = Uniqid()                                         //你自己的商品订单号，不能重复
-var payAmount float64 = 0.01                                             //付款金额，单位:元
-var orderName string = "支付测试"                                            //订单标题
-var signType string = "RSA2"                                             //签名算法类型，支持RSA2和RSA，推荐使用RSA2
+var appid string = "xxxxx"                              //https://open.alipay.com 账户中心->密钥管理->开放平台密钥，填写添加了当面付应用的APPID
+var notifyUrl string = "http://www.example.com/notify"	//付款成功后的异步回调地址
+var payAmount float64 = 0.01                                       //付款金额，单位:元
+var orderName string = "支付测试"                                      //订单标题
+var signType string = "RSA2"                                       //签名算法类型，支持RSA2和RSA，推荐使用RSA2
 
 //商户私钥，填写对应签名算法类型的私钥，如何生成密钥参考：https://docs.open.alipay.com/291/105971和https://docs.open.alipay.com/200/105310
-var rsaPrivateKey string = "MIIEpAIBAAKCAQEA1MV+OY6MvGfXPM0MkpjT+FdzGmPOvVmX2wF3gjwQpeHBEUP9jLXhVS32fZ1iXI1e7WUGQ5tvXn28P8190kpOn/c/G5t2CAksUvemvF7uJN/N3Z1HFMdt3omvCd14K05lgcFYz7Z4c+A7ZJF5bPCB6oshjjUmbCY3hibuWzX/1j8AgsoD9lLyxoFqxLj98k5ZrYIhk900gMQs/WJ3A1FC09Dln9fuhBUyjtPHaml+4w+sdkdzxPktxdFrMcI7M7rNEwg25XtST5Z49oFpE84AlXM7+oC9jYvIpTGE00WomsgtakN039ucT/59Bup6pLkO08Rv85UXbqzGTcYAhNHLfQIDAQABAoIBAQCbuPM58s+j8KgB8ty5yiqRPoeaj+O2h4Txn7A02/sfPQvNtCI0wsTpT5twsihULo+EVYTxJCitUn7df2sP5pyGzTEd5njLRtNu4Zvhj+Thjf8grERiu9b4oXI/WRzjLRxzi+uREi40OK+fWi0xgxDCdROY/eNiEdJfV8zpaqsUxG7VdwZIJQ/8d3Mi31OWv30kr9jfEd15DBInGJgSqR+qwrAB4pBSMcW8hL6PYlzoPi1ygceFjRrnbeMG40zt0OUPSexQIgAmFvGqxTl5xo3dFEziGHdfWYsBKZ2M8ubAe+R6LcndxI+o2Hw4TNcC1tDeNMtjw7+h9S5aef5A8uWBAoGBAPxCLWPhUHCYlIXUz0D1SoolZs9WK7Kz1YSWnzqrpegN+foS5/ji93YylGE+KL31TwbnGQLAwknwMX3qTzmkvTovmy8jevXBsCSEFm81q0wG/35e1SKkTXL66RqB2y0xFLdcF3f9s8ZiEclqkYwNSHh0nqzREfIxMMAsj+3n2vHdAoGBANftYkZYrbs4iI/ZcjmBYguYikNfNmrD+Ta6ckOGZqsHfwXJCAz1rF4/XCqVAc9nxuzJR/72qkn9z07uH6qSZCqlZDRkiiKaK2UVqFDB+0abMk/TGHXuMmdvMkyj2jEZxG2rkg0kmg4qYkkg/5tGG1On/0GeZNVPu8JpsFr1pDYhAoGBANr8pCTKC6fDfWP1C3qrtmrY7zhc6RB4d4pjq5UmP5+EypaiZQi2F/dfD1qfuIS3eURXyGmQZtoDDyPtDZvP/ImPnFs+pNbFryD0HfmrEKquhIvyzXoGQknnsgbV5iyEKCTJaII9FxzINAKzZei7+0a+jqUd1kN3Gogp50Sze2ltAoGARaM5Xpaa8RZ6dGocfI9Nn4/Ch5fdZPFvHkdjMoPV+LKiNKtw/Tz+KiclAlasDsfZT+RaY9AJe3NvuHTzoX807swIVR1Xr3EpLaCed+0XrN3AjB34dZAskU87WZw+cjdtMjFzGOoFBSyGJi+OP/WMOp6jo/YBbwoX88tCJROzsgECgYAT8pHHIyPt5Y/5pDb8EDvD3XNES1fBkfZffSoAodsrkeoKgrsKl+9M3rcGX+S9dscyoH0ur3BFTMHtIOOhC5qytt+BhMHIP5mAs4di4u/joQCWQbUyrUggVK5it+6BFgAT+jeB7zTAUtgGpTVFq3kLbV0NZ+XQyEHVlnoJnHYpQg=="
+var rsaPrivateKey string = "xxxxx"
+
+//支付宝公钥，登录支付宝开放平台，账户中心->密钥管理->开放平台密钥，找到对应的应用，在接口内容加密方式处查看支付宝公钥
+var alipayPublicKey string = "xxxxx"
 
 /*** 配置结束 ***/
 
 type AlipayService struct {
-	appId         string
-	returnUrl     string
-	notifyUrl     string
-	charset       string
-	rsaPrivateKey *rsa.PrivateKey
-	totalFee      float64
-	outTradeNo    string
-	orderName     string
+	appId           string
+	notifyUrl       string
+	charset         string
+	rsaPrivateKey   *rsa.PrivateKey
+	alipayPublicKey *rsa.PublicKey
+	totalFee        float64
+	outTradeNo      string
+	orderName       string
 }
 
 func (this *AlipayService) SetAppId(appId string) {
 	this.appId = appId
-}
-
-func (this *AlipayService) SetReturnUrl(returnUrl string) {
-	this.returnUrl = returnUrl
 }
 
 func (this *AlipayService) SetNotifyUrl(notifyUrl string) {
@@ -62,49 +60,21 @@ func (this *AlipayService) SetCharset(charset string) {
 	this.charset = charset
 }
 
-// FormatPrivateKey 格式化 普通应用秘钥
-func (this *AlipayService) FormatPrivateKey(privateKey string) (pKey string) {
-	var buffer strings.Builder
-	buffer.WriteString("-----BEGIN RSA PRIVATE KEY-----\n")
-	rawLen := 64
-	keyLen := len(privateKey)
-	raws := keyLen / rawLen
-	temp := keyLen % rawLen
-	if temp > 0 {
-		raws++
-	}
-	start := 0
-	end := start + rawLen
-	for i := 0; i < raws; i++ {
-		if i == raws-1 {
-			buffer.WriteString(privateKey[start:])
-		} else {
-			buffer.WriteString(privateKey[start:end])
-		}
-		buffer.WriteByte('\n')
-		start += rawLen
-		end = start + rawLen
-	}
-	buffer.WriteString("-----END RSA PRIVATE KEY-----\n")
-	pKey = buffer.String()
-	return
-}
+func (this *AlipayService) SetRsaPrivateKey(keyString string) {
 
-func (this *AlipayService) SetRsaPrivateKey(pkcs1keyStr string) {
-
-	var block *pem.Block
-	var privateKey *rsa.PrivateKey
-	var err error
-	pk := this.FormatPrivateKey(pkcs1keyStr)
-	if block, _ = pem.Decode([]byte(pk)); block == nil {
-		panic("pem.Decode：privateKey decode error")
-	}
-
-	if privateKey, err = x509.ParsePKCS1PrivateKey(block.Bytes); err != nil {
+	privateKey, err := ParsePrivateKey(FormatPrivateKey(keyString))
+	if err != nil {
 		panic(err)
 	}
-
 	this.rsaPrivateKey = privateKey
+}
+
+func (this *AlipayService) SetAlipayPublicKey(keyString string) {
+	alipayPublicKey, err := ParsePublicKey(FormatPublicKey(keyString))
+	if err != nil {
+		panic(err)
+	}
+	this.alipayPublicKey = alipayPublicKey
 }
 
 func (this *AlipayService) SetTotalFee(totalFee float64) {
@@ -130,8 +100,6 @@ func (this *AlipayService) GenSign(m map[string]string) string {
 	}
 	sort.Strings(data)
 	signData := strings.Join(data, "&")
-	//println(signData)
-	//return ""
 	s := sha256.New()
 	_, err := s.Write([]byte(signData))
 	if err != nil {
@@ -146,23 +114,11 @@ func (this *AlipayService) GenSign(m map[string]string) string {
 	return base64.StdEncoding.EncodeToString(encryptedBytes)
 }
 
-func (this *AlipayService) buildRequestForm(m map[string]string) string {
-	var buf []string
-	buf = append(buf, fmt.Sprintf("正在跳转至支付页面...<form id='alipaysubmit' name='alipaysubmit' action='https://openapi.alipay.com/gateway.do?charset=%s' method='POST'>", this.charset))
-	for k, v := range m {
-		buf = append(buf, fmt.Sprintf("<input type='hidden' name='%s' value='%s'/>", k, v))
-	}
-	buf = append(buf, "<input type='submit' value='ok' style='display:none;''></form>")
-	buf = append(buf, "<script>document.forms['alipaysubmit'].submit();</script>")
-	return strings.Join(buf, "")
-}
-
-func (this AlipayService) DoPay() (string, error) {
+func (this *AlipayService) DoPay() (string, error) {
 
 	//请求参数
 	var bizContent = make(map[string]string)
 	bizContent["out_trade_no"] = this.outTradeNo
-	bizContent["product_code"] = "FAST_INSTANT_TRADE_PAY"
 	bizContent["total_amount"] = strconv.FormatFloat(float64(this.totalFee), 'f', 2, 64) //2表示保留2位小数
 	bizContent["subject"] = this.orderName
 	bizContentJson, err := json.Marshal(bizContent)
@@ -175,7 +131,6 @@ func (this AlipayService) DoPay() (string, error) {
 	m["app_id"] = this.appId
 	m["method"] = "alipay.trade.precreate" //接口名称
 	m["format"] = "JSON"
-	m["return_url"] = this.returnUrl
 	m["charset"] = this.charset
 	m["sign_type"] = "RSA2"
 	m["timestamp"] = time.Now().Format("2006-01-02 15:04:05")
@@ -190,6 +145,129 @@ func (this AlipayService) DoPay() (string, error) {
 	return postData(requestUrl, m), nil
 }
 
+func (this *AlipayService) VerifySign(data url.Values) (ok bool, err error) {
+	return verifySign(data, this.alipayPublicKey)
+}
+
+func ParsePrivateKey(data []byte) (key *rsa.PrivateKey, err error) {
+	var block *pem.Block
+	block, _ = pem.Decode(data)
+	if block == nil {
+		return nil, errors.New("private key failed to load")
+	}
+
+	var priInterface interface{}
+	priInterface, err = x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	key, ok := priInterface.(*rsa.PrivateKey)
+	if !ok {
+		return nil, errors.New("private key failed to load")
+	}
+
+	return key, err
+}
+
+func ParsePublicKey(data []byte) (key *rsa.PublicKey, err error) {
+	var block *pem.Block
+	block, _ = pem.Decode(data)
+	if block == nil {
+		return nil, errors.New("alipay public key failed to load")
+	}
+
+	var pubInterface interface{}
+	pubInterface, err = x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	key, ok := pubInterface.(*rsa.PublicKey)
+	if !ok {
+		return nil, errors.New("alipay public key failed to load")
+	}
+
+	return key, err
+}
+
+func FormatPublicKey(raw string) []byte {
+	return formatKey(raw, "-----BEGIN PUBLIC KEY-----", "-----END PUBLIC KEY-----", 64)
+}
+
+func FormatPrivateKey(raw string) []byte {
+	return formatKey(raw, "-----BEGIN RSA PRIVATE KEY-----", "-----END RSA PRIVATE KEY-----", 64)
+}
+
+func formatKey(raw, prefix, suffix string, lineCount int) []byte {
+	if raw == "" {
+		return nil
+	}
+	raw = strings.Replace(raw, prefix, "", 1)
+	raw = strings.Replace(raw, suffix, "", 1)
+	raw = strings.Replace(raw, " ", "", -1)
+	raw = strings.Replace(raw, "\n", "", -1)
+	raw = strings.Replace(raw, "\r", "", -1)
+	raw = strings.Replace(raw, "\t", "", -1)
+
+	var sl = len(raw)
+	var c = sl / lineCount
+	if sl%lineCount > 0 {
+		c = c + 1
+	}
+
+	var buf bytes.Buffer
+	buf.WriteString(prefix + "\n")
+	for i := 0; i < c; i++ {
+		var b = i * lineCount
+		var e = b + lineCount
+		if e > sl {
+			buf.WriteString(raw[b:])
+		} else {
+			buf.WriteString(raw[b:e])
+		}
+		buf.WriteString("\n")
+	}
+	buf.WriteString(suffix)
+	return buf.Bytes()
+}
+
+func verifySign(data url.Values, key *rsa.PublicKey) (ok bool, err error) {
+	sign := data.Get("sign")
+
+	var keys = make([]string, 0, 0)
+	for key := range data {
+		if key == "sign" || key == "sign_type" {
+			continue
+		}
+		keys = append(keys, key)
+	}
+
+	sort.Strings(keys)
+
+	var pList = make([]string, 0, 0)
+	for _, key := range keys {
+		pList = append(pList, key+"="+data.Get(key))
+	}
+	var s = strings.Join(pList, "&")
+
+	return verifyData([]byte(s), sign, key)
+}
+
+func verifyData(data []byte, sign string, key *rsa.PublicKey) (ok bool, err error) {
+	signBytes, err := base64.StdEncoding.DecodeString(sign)
+	if err != nil {
+		return false, err
+	}
+
+	var h = crypto.SHA256.New()
+	h.Write(data)
+	var hashed = h.Sum(nil)
+
+	if err = rsa.VerifyPKCS1v15(key, crypto.SHA256, hashed, signBytes); err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
 func postData(requestUrl string, m map[string]string) string {
 	data := make(url.Values)
 	for k, v := range m {
@@ -202,8 +280,6 @@ func postData(requestUrl string, m map[string]string) string {
 		return "请求错误"
 	}
 	defer resp.Body.Close()
-	// body, _ := ioutil.ReadAll(resp.Body)
-	// fmt.Println(string(body))
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(resp.Body)
 	return buf.String()
@@ -215,13 +291,25 @@ func Uniqid() string {
 	return fmt.Sprintf("%s%08x%05x", "", now.Unix(), now.UnixNano()%0x100000)
 }
 
-func pcPay(w http.ResponseWriter, r *http.Request) {
+func doPay(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+	if params.Get("total_fee") != "" {
+		payAmount, _ = strconv.ParseFloat(params.Get("total_fee"), 64)
+	}
+	outTradeNo := Uniqid()
+	if params.Get("out_trade_no") != "" {
+		outTradeNo = params.Get("out_trade_no")
+	}
+	if params.Get("order_name") != "" {
+		orderName = params.Get("order_name")
+	}
+
 	aliPay := AlipayService{}
 	aliPay.SetAppId(appid)
-	aliPay.SetReturnUrl(returnUrl)
 	aliPay.SetNotifyUrl(notifyUrl)
 	aliPay.SetCharset("utf-8")
 	aliPay.SetRsaPrivateKey(rsaPrivateKey)
+	aliPay.SetAlipayPublicKey(alipayPublicKey)
 	aliPay.SetTotalFee(payAmount)
 	aliPay.SetOutTradeNo(outTradeNo)
 	aliPay.SetOrderName(orderName)
@@ -229,23 +317,74 @@ func pcPay(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-	// $result = $result['alipay_trade_precreate_response'];
-	// if($result['code'] && $result['code']=='10000'){
-	// 	//生成二维码
-	// 	$url = 'https://sapi.k780.com/?app=qr.get&level=H&size=6&data='.$result['qr_code'];
-	// 	echo "<img src='{$url}' style='width:300px;'><br>";
-	// 	echo '二维码内容：'.$result['qr_code'];
-	// }else{
-	// 	echo $result['msg'].' : '.$result['sub_msg'];
-	// }
-	// w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, response)
+	res := []byte(response)
+	var data interface{}
+	err = json.Unmarshal(res, &data)
+	if err != nil {
+		fmt.Fprintf(w, "JSON解码失败:%v\n", err)
+		return
+	}
+
+	//访问解码后的数据
+	dataDecode, ok := data.(map[string]interface{})
+	if ok {
+		alipayResponse := dataDecode["alipay_trade_precreate_response"].(map[string]interface{})
+		code := alipayResponse["code"].(string)
+		if code == "10000" {
+			//生成二维码
+			qrCode := alipayResponse["qr_code"].(string)
+			url := "http://api.qrserver.com/v1/create-qr-code/?size=300x300&margin=10&data=" + qrCode
+			html := "<img src='" + url + "' style='width:300px;'><br>二维码内容：" + qrCode
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			fmt.Fprintf(w, html)
+			return
+		} else {
+			msg := alipayResponse["msg"].(string)
+			subMsg := alipayResponse["sub_msg"].(string)
+			fmt.Fprintf(w, msg+" : "+subMsg)
+			return
+		}
+	}
+
+	fmt.Fprintf(w, "支付接口出错")
+}
+
+type Book struct {
+	Id      int    `json:"id"`
+	Title   string `json:"title"`
+	Summary string `json:"summary"`
+	Author  string `json:"author"`
+}
+
+//异步回调通知处理
+func notify(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	//接收post参数
+	r.ParseForm()
+	formdata := r.PostForm
+
+	aliPay := AlipayService{}
+	aliPay.SetCharset("utf-8")
+	aliPay.SetAlipayPublicKey(alipayPublicKey)
+	_, err := aliPay.VerifySign(formdata)
+	if err != nil {
+		fmt.Fprintf(w, "error:"+err.Error())
+		return
+	}
+	//处理你的逻辑，例如获取订单号formdata["out_trade_no"][0]，订单金额formdata["total_amount"][0]等
+	//例如这里将回调数据写入到nofity.txt文本中
+
+	ioutil.WriteFile("nofity.txt", []byte(formdata.Encode()), 0644)
+	//程序执行完后必须打印输出“success”（不包含引号）。如果商户反馈给支付宝的字符不是success这7个字符，支付宝服务器会不断重发通知，直到超过24小时22分钟。一般情况下，25小时以内完成8次通知（通知的间隔频率一般是：4m,10m,10m,1h,2h,6h,15h）；
+	fmt.Fprintf(w, "success")
 }
 
 func main() {
 	//开启web服务器
 	port := "8080"
-	http.HandleFunc("/", pcPay)
+	http.HandleFunc("/", doPay)
+	http.HandleFunc("/notify", notify)
 	log.Println("服务器启动成功，监听端口：", port)
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
